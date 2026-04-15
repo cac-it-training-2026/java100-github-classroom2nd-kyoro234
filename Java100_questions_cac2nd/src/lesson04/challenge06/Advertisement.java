d8e374210de392a87732be934df23e5386309b58
kminemu <kminemu@C010423626.zipangu.cac.co.jp>
2026-04-15 17:00:08 +0900

    lesson1 challenge10

---
+++ Java100_questions_cac2nd/src/lesson04/challenge06/Advertisement.java
@@ -0,0 +1,21 @@
+package lesson04.challenge06;
+
+public class Advertisement {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		System.out.println("ポエールエルメ一緒に働いてくれるクルー募集中！");
+		System.out.println("私たちと一緒に働きませんか？");
+		System.out.println("勤務時間：10時～15時・15時～20時");
+		System.out.println("時給：1230円");
+		System.out.println("待遇：制服貸与・交通費支給・まかない制度あり");
+		
+		System.out.println("皆さんの応募をお待ちしております！");
+		
+		
+		
+
+	}
+
+}
